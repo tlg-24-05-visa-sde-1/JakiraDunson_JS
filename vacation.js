acf940d1455639b8444ad4925b76407f4e1927f6
Jakira Dunson <jakiradunson@gmail.com>
2024-07-03 14:15:41 -0500

    changes to HTML and CSS

--- vacation.js
+++ vacation.js
@@ -1,23 +1,43 @@
 //doocument what is being selected
 document.addEventListener("DOMContentLoaded", function () {
-  // Select the button using its ID
-  const submitButton = document.querySelector("#submitButton");
-  const destinationName = document.querySelector("#destinationName");
-  const location = document.querySelector("#location");
-  const photo = document.querySelector("#photo");
+  document
+    .querySelector("#destinationName")
+    .addEventListener("submit", submittedForm);
 
-  // Add an event listener to the button
-  submitButton.addEventListener("click", function (event) {
-    // Prevent the default form submission behavior
-    event.preventDefault(); //prevents the default behavior of button once it is clicked
+  function submittedForm(event) {}
+  //this fucntion focuses on the destination form
+  event.preventDefault(); //no refresh
 
-    // Call the function to handle the button click
-    handleButtonClick();
-  });
+  //get the values off the webpage
 
-  // Define the function to handle the button click
-  function handleButtonClick() {
-    console.log("Button was clicked!");
-    // Your logic for handling the button click goes here
-  }
+  var destination = event.target.elements["destination"].value;
+  var location = event.target.elements["location"].value;
+  var photo = event.target.elements["photo"].value;
+  var description = event.target.elements["description"].value;
+
+  resetFormValues(event.target);
+
+  var newDestinationCard = createDestinationCard(
+    destination,
+    location,
+    photo,
+    description
+  );
+
+  var wishListContainer = document.querySelector("#destinationName");
+
+  //   // Add an event listener to the button
+  //   submitButton.addEventListener("click", function (event) {
+  //     // Prevent the default form submission behavior
+  //     event.preventDefault(); //prevents the default behavior of button once it is clicked
+
+  //     // Call the function to handle the button click
+  //     handleButtonClick();
+  //   });
+
+  //   // Define the function to handle the button click
+  //   function handleButtonClick() {
+  //     console.log("Button was clicked!");
+  //     // Your logic for handling the button click goes here
+  //   }
 });
